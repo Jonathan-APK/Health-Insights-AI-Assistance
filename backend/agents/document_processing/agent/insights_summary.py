bda1353ecacb85f149ccf890f1af761e2f13f3cb
Loots <wang_lu_ting@hotmail.com>
2026-04-03 12:41:22 +0800

    Langfuse integration
    changed app redis port number to 7001 to prevent conflict with langfuse docker; added langfuse in uv; added dotenv in uv; added LLM tracing and prompt management on "insights_summary" agent;

--- backend/agents/document_processing/agent/insights_summary.py
+++ backend/agents/document_processing/agent/insights_summary.py
@@ -1,20 +1,33 @@
 import logging
+import os
 from datetime import datetime
 from zoneinfo import ZoneInfo
 
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_openai import ChatOpenAI
+from langfuse import observe
+from langfuse import get_client
+from langfuse import Langfuse
+from dotenv import load_dotenv
 
 from config.settings import settings
 from core.prompt_loader import load_prompt_config
 
+load_dotenv()
+
 logger = logging.getLogger("insights_summary")
+langfusePrompt = Langfuse(
+    public_key=os.getenv("LANGFUSE_PUBLIC_KEY"),
+    secret_key=os.getenv("LANGFUSE_SECRET_KEY"),
+    host=os.getenv("LANGFUSE_BASE_URL")
+)
+langfuse = get_client()
 
 
 def now():
     return datetime.now(ZoneInfo("Asia/Singapore")).isoformat()
 
-
+@observe(as_type="generation")
 def insights_summary_node(state):
     """
     Generate insight summary from report text using LLM.
@@ -29,7 +42,12 @@ def insights_summary_node(state):
             module="insights_summary", key="summarize", version=version
         )
 
-        system_prompt = analysis_config["system"]
+        # langfuse prompt managment (START)
+        promptfromLangfuse = langfusePrompt.get_prompt("insights_summary/systemPrompt")
+        system_prompt = promptfromLangfuse.compile()
+        ## system_prompt = analysis_config["system"]
+        # langfuse prompt managment (END)
+
         model = analysis_config["model"]
         temperature = analysis_config["temperature"]
 
@@ -42,12 +60,21 @@ def insights_summary_node(state):
 
         # Call LLM for classification with config from prompts.json
         llm = ChatOpenAI(model=model, temperature=temperature)
-        result = llm.invoke(
+        response = llm.invoke(
             [
                 SystemMessage(content=system_prompt),
                 HumanMessage(content=combined_content),
             ]
-        ).content.strip()
+        )
+
+        # Update langfuse monitoring w/ prompt management
+        langfuse.update_current_generation(
+            usage_details=response.response_metadata.get("token_usage"),
+            model=response.response_metadata.get("model_name"),
+            prompt=promptfromLangfuse
+        )
+
+        result = response.content.strip()
 
         if state.input_text:
             logger.info("User entered text, past to QnA agent for further processing.")
